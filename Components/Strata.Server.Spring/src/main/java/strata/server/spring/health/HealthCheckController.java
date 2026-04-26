03c507eb1c25b69a2bc179a4761c93dfd69bdd32
johnliebenau <johnliebenau@gmail.com>
2026-04-26 08:14:24 -0700

    Modified HealthCheckController to have a configurable base path if needed.

--- Components/Strata.Server.Spring/src/main/java/strata/server/spring/health/HealthCheckController.java
+++ Components/Strata.Server.Spring/src/main/java/strata/server/spring/health/HealthCheckController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("${strata.health.base-path:/health}")
 public
 class HealthCheckController
 {
